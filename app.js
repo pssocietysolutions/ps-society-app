3d181fd37381cc0046f221efd5d2c9f68440b871
pssocietysolutions <ps.societysolutions@gmail.com>
2026-08-19 23:30:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -909,8 +909,6 @@ async function saveFCMTokenToSupabase(token) {
   }
 }
 
-import { getMessaging, onMessage } from "firebase/messaging";
-
 const messaging = getMessaging();
 onMessage(messaging, (payload) => {
   console.log('Message received in foreground: ', payload);
