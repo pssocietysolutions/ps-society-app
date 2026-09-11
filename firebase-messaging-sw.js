f70f8ea1af1a321104f19821726d232d14d943ec
pssocietysolutions <ps.societysolutions@gmail.com>
2026-09-11 14:14:07 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 // firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
 console.log("🔥 Service Worker Loaded Successfully!");
 
 firebase.initializeApp({
