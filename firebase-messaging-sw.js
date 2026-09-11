25b372622cf58209ef6978239d4b8ee30b0dbd6f
pssocietysolutions <ps.societysolutions@gmail.com>
2026-09-11 14:27:50 +0530

    Upgrade Firebase SDK version in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 console.log("🔥 Service Worker Loaded Successfully!");
 
