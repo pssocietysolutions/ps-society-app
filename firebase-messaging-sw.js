a26398cf37b63153f0f5f9d2c18c4f908572cad9
pssocietysolutions <ps.societysolutions@gmail.com>
2026-09-11 12:55:29 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -39,7 +39,7 @@ self.addEventListener('notificationclick', function(event) {
   let urlToOpen = data.click_action || data.url || '/';
 
   if (!urlToOpen.startsWith('http')) {
-    const baseUrl = self.location.origin;
+   const baseUrl = self.registration.scope;
     urlToOpen = baseUrl + urlToOpen;
   }
 
